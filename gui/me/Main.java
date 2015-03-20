c87df47e92485902cea388a137af7a1496bcabf3
feihuanwuying <1397645429@qq.com>
2015-03-20 13:18:26 +0800

    push for test

--- gui/me/Main.java
+++ gui/me/Main.java
@@ -1,13 +1,14 @@
 ﻿package me;
+
 import java.io.IOException;
 import me.calendar.frame.MainFrame;
 
 public class Main {
 
-	 public static void main(String[] args) throws IOException//主程式開始
-	  {
-	    MainFrame inst = new MainFrame();
-	    inst.setVisible(true);
-	  }
+	public static void main(String[] args) throws IOException// 主程式開始
+	{
+		MainFrame inst = new MainFrame();
+		inst.setVisible(true);
+	}
 
 }
