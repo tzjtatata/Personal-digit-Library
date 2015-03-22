73195b3f6fae7c43331c032826c3d21fac8aee43
feihuanwuying <1397645429@qq.com>
2015-03-23 01:18:53 +0800

    完善Search.java

--- gui/backtable/Search.java
+++ gui/backtable/Search.java
@@ -1,10 +1,15 @@
 package backtable;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 集合了搜索方法的类
@@ -32,10 +37,11 @@ public class Search {
 					if (flist1.isFile() && !flist1.isHidden()) {  //判断文件
 						if (flist1.getName().endsWith("txt")) {
 							MakeFile(flist1, notFirst, "txtFolder\\");
+							notFirst = true;  //将标志量置为true，下次写之后的循环中写文件将不再清空
 						} else if (flist1.getName().endsWith("pdf") || flist1.getName().endsWith("doc")) {
 							MakeFile(flist1, notFirst, "otherFolder\\");
+							notFirst = true;
 						}
-						notFirst = true;  //将标志量置为true，下次写之后的循环中写文件将不再清空
 					}
 				}
 			}
@@ -53,15 +59,66 @@ public class Search {
 	 * @throws IOException
 	 */
 	public void MakeFile(File f, boolean nf, String type) throws IOException {
-		boolean notFirst = nf;
 		//创建文件
 		File file = new File(System.getProperty("java.class.path") + "\\backtable\\" + type + f.getParent().replaceAll("\\\\", "@@").replaceAll(":@@", "@@@"));
 		if (!file.exists()) {
 			file.createNewFile();
 		}
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, notFirst)));
-		notFirst = true;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, nf)));
 		bw.write(f.getName() + '\n');
 		bw.close();
+	}
+
+	/**
+	 * 根据传入的正则表达式进行目录搜索
+	 *
+	 * @param path 搜索路径
+	 * @param p 正则表达式
+	 * @return 内容为文件路径的动态数组
+	 * @throws Exception
+	 */
+	public ArrayList<String> SearchDish(String path, Pattern p) throws Exception {
+		ArrayList<String> al = new ArrayList<>();
+		try {
+			File f = new File(path);
+			if (f.isDirectory() && f.listFiles().length != 0) {
+				File flist[] = f.listFiles();
+				for (File flist1 : flist) {
+					if (!flist1.isHidden() && flist1.isDirectory() && flist1.listFiles().length != 0) {
+						SearchDish(flist1.getPath());
+					}
+					if (flist1.isFile() && !flist1.isHidden()) {
+						FileReader fr = new FileReader(flist1);
+						BufferedReader br = new BufferedReader(fr);
+						while (br.ready()) {
+							String s = br.readLine();
+							Matcher m = p.matcher(s);
+							if (m.find()) {
+								al.add(flist1.getName().replaceAll("@@@", ":@@").replaceAll("@@", "\\\\") + s);
+							}
+						}
+						br.close();
+						fr.close();
+					}
+				}
+			}
+		} catch (Exception e) {
+		}
+		return al;
+	}
+
+	/**
+	 * 搜索题目
+	 *
+	 * @param name 要搜索的名字
+	 * @param type 文件类型
+	 * @throws Exception
+	 */
+	public void NameSearch(String name, String type) throws Exception {
+		Pattern p = Pattern.compile(name);
+		//暂时以打印为输出
+		for (String sd : SearchDish(System.getProperty("java.class.path") + "\\backtable\\" + type, p)) {
+			System.out.println(sd);
+		}
 	}
 }
