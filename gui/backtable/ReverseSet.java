41ec7f2f41e44b60a83c23ae8f89acdd5ecbc6b0
feihuanwuying <1397645429@qq.com>
2015-03-25 22:56:05 +0800

    由于第三方包的引入，更改图片地址加载方式

--- gui/backtable/ReverseSet.java
+++ gui/backtable/ReverseSet.java
@@ -5,13 +5,16 @@ import java.io.*;
 import backtable.Hashstr;
 
 public class ReverseSet {
+
 	//获取txtFolder文件夹下的所有文件
-	protected File ft = new File(System.getProperty("java.class.path")+"/backtable/txtFolder");
+
+	protected File ft = new File("gui/backtable/txtFolder");
 	protected File[] ls = ft.listFiles();
 	static int round = 0x500;
-	int i ;
+	int i;
 	public long[] cryptTable = new long[round];
-	Hashstr[] lpTable = new  Hashstr[round];
+	Hashstr[] lpTable = new Hashstr[round];
+
 	public ReverseSet() throws IOException {
 		//prepareCryptTable();
 		//对目录下每个txt读取，获取子文件目录
@@ -22,89 +25,91 @@ public class ReverseSet {
 		//最后将hash数组输出到倒排索引表中
 		printf();
 	}
+
 	public void Refile() throws FileNotFoundException {
 		long temp = 0;
 		int i;
-		String[] word =  {"zoukaifa","dadoubi","dahuaidan","kosting","zoukaifa","dadoubi","zoukaifa","dadoubi","liyuanze"};
+		String[] word = {"zoukaifa", "dadoubi", "dahuaidan", "kosting", "zoukaifa", "dadoubi", "zoukaifa", "dadoubi", "liyuanze"};
 		//分词
 		String ffi = "test.txt";
 		//prepareCryptTable();
-		for (i = 0;i<word.length;i++)
-		{	
-		/*对每个词求其hash值，存进对应的数组项
-		 * 并用链表存储其所在地址
-		*/
-		setpos(word[i],ffi);
+		for (i = 0; i < word.length; i++) {
+			/*对每个词求其hash值，存进对应的数组项
+			 * 并用链表存储其所在地址
+			 */
+			setpos(word[i], ffi);
 		}
 	}
+
 	public void prepareCryptTable() {
-			long seed = 0x001000001L ,  index1 = 0 , index2 = 0 ;
-			int i;
-			for (index1 = 0;index1<0x100;index1++){
-				for (index2 = index1, i = 0;i<5;i++,index2 += 0x100){
-					long temp1,temp2;
-					seed = (seed *125 + 3) % 0x2AAAABL;
-					temp1 = (seed & 0xFFFF) << 0x10;
-					seed = (seed *125 + 3) % 0x2AAAABL;
-					temp2= (seed & 0xFFFF) ;
-					//System.out.println(index2);
-					cryptTable[(int)index2] = ( temp1 | temp2);
-				}
+		long seed = 0x001000001L, index1 = 0, index2 = 0;
+		int i;
+		for (index1 = 0; index1 < 0x100; index1++) {
+			for (index2 = index1, i = 0; i < 5; i++, index2 += 0x100) {
+				long temp1, temp2;
+				seed = (seed * 125 + 3) % 0x2AAAABL;
+				temp1 = (seed & 0xFFFF) << 0x10;
+				seed = (seed * 125 + 3) % 0x2AAAABL;
+				temp2 = (seed & 0xFFFF);
+				//System.out.println(index2);
+				cryptTable[(int) index2] = (temp1 | temp2);
 			}
+		}
 	}
 	/*public long HashString(String key) {
-		long seed1 = 0x7FED7FEDL;
-		long seed2 = 0xEEEEEEEEL;
-		//System.out.println(seed1 + "," +seed2);
-		int ch;
-		int len = key.length(),i;
-		for (i = 0;i<len;i++){
-			ch = key.charAt(i);
-			seed1 = cryptTable[(1<<8) + ch] ^ (seed1 + seed2);
-			seed2 = ch + seed1 + seed2 + (seed2<<5) + 3; 
-		}
-		return seed1;
-	}*/
+	 long seed1 = 0x7FED7FEDL;
+	 long seed2 = 0xEEEEEEEEL;
+	 //System.out.println(seed1 + "," +seed2);
+	 int ch;
+	 int len = key.length(),i;
+	 for (i = 0;i<len;i++){
+	 ch = key.charAt(i);
+	 seed1 = cryptTable[(1<<8) + ch] ^ (seed1 + seed2);
+	 seed2 = ch + seed1 + seed2 + (seed2<<5) + 3;
+	 }
+	 return seed1;
+	 }*/
+
 	public long HashString(String key) {
 		int ch;
-		int len = key.length(),i;
+		int len = key.length(), i;
 		long Hash = 0;
-		for (i = 0;i<len;i++){
+		for (i = 0; i < len; i++) {
 			ch = key.charAt(i);
 			Hash = Hash * 33 + ch;
 		}
 		return Hash;
 	}
-	public void printf() throws IOException
-	{
+
+	public void printf() throws IOException {
 		int i;
 		File fi = new File("gui/backtable/daopai.pdl");
 		fi.createNewFile();
 		FileWriter fw = new FileWriter(fi);
-		for (i = 0;i<round;i++) {
-			if (lpTable[i] != null){
+		for (i = 0; i < round; i++) {
+			if (lpTable[i] != null) {
 				fw.write(lpTable[i].toString());
 			}
 		}
 		fw.close();
 	}
-	public void setpos(String lpString,String file){
+
+	public void setpos(String lpString, String file) {
 		int temp;
 		long nHash = HashString(lpString);
 		//System.out.println(nHash);
-		temp = (int)(HashString(lpString) % round);
+		temp = (int) (HashString(lpString) % round);
 		//System.out.println(lpString +  ", *** " +nHash);
-		if (lpTable[temp] == null)
-		{
-			lpTable[temp]= new Hashstr(nHash,file);
+		if (lpTable[temp] == null) {
+			lpTable[temp] = new Hashstr(nHash, file);
 			//System.out.println(lpString +  ",  " +nHash);
-		}
-		else {
-			lpTable[temp].addhash(nHash,file);
+		} else {
+			lpTable[temp].addhash(nHash, file);
 			//System.out.println("!" + lpString);
 		}
 	}
+
 	public static void main(String[] args) throws IOException {
-			new ReverseSet();
+		new ReverseSet();
 	}
 }
