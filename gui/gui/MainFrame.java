83da358fbce64aba9dca46a20362eb4add1cd780
feihuanwuying <1397645429@qq.com>
2015-04-26 12:54:38 +0800

    暂定以gui包为新的gui类的所属包

---
+++ gui/gui/MainFrame.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * JFrame,各个JPanel的容器
+ * @author 开发
+ */
+public class MainFrame extends JFrame{
+    
+}
