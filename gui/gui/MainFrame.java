49279b7db084de2eb5ecf5602f6e094786a424bf
feihuanwuying <1397645429@qq.com>
2015-04-26 13:10:04 +0800

    暂定以gui包为新的gui类的所属包

--- gui/gui/MainFrame.java
+++ gui/gui/MainFrame.java
@@ -17,5 +17,13 @@ public class MainFrame extends JFrame{
     public MainFrame(){
 		this.setIconImage(this.getToolkit().getImage("gui/source/digital_library.png"));  //logo
         this.setContentPane(mainJPanel);  //放置
+        this.setFocusTraversalPolicyProvider(false);  //暂时没用,似乎可以取消tab键控制焦点
+        this.setBounds(200, 50, 950, 650);
+        this.setResizable(false);  //大小不可变
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new MainFrame();
     }
 }
