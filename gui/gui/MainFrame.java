77f711e41e92c8252d42d05f4625735052fa8862
feihuanwuying <1397645429@qq.com>
2015-04-26 12:59:05 +0800

    暂定以gui包为新的gui类的所属包

--- gui/gui/MainFrame.java
+++ gui/gui/MainFrame.java
@@ -14,5 +14,10 @@ import java.awt.event.*;
  * @author 开发
  */
 public class MainFrame extends JFrame{
-    
+    JPanel mainJPanel = new JPanel();  //主JPanel
+    CardLayout cl = new CardLayout();
+    public MainFrame(){
+		this.setIconImage(this.getToolkit().getImage("gui/source/digital_library.png"));  //logo
+        this.setContentPane(mainJPanel);  //放置
+    }
 }
