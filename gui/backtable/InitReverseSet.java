a32f5ae0e28c31dd9e3a2e73ee9c62978390f0ea
feihuanwuying <1397645429@qq.com>
2015-07-15 19:54:45 +0800

    还原一些作死的操作。

--- gui/backtable/InitReverseSet.java
+++ gui/backtable/InitReverseSet.java
@@ -13,10 +13,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class InitReverseSet {
-
-    private HashMap<BigInteger, ArrayList<BigInteger>> data = new HashMap<>();
-    private ArrayList<String> Paths = new ArrayList<>();
-    public CountDownLatch threadSignal;
+        private HashMap<BigInteger,ArrayList<BigInteger>> data = new HashMap<>();
+        private ArrayList<String> Paths = new ArrayList<>();
+        public CountDownLatch threadSignal;
 //	public void change() {
 //		if (System.getProperty("os.name").startsWith("W")) {
 //			len = "\\\\";
@@ -25,57 +24,55 @@ public class InitReverseSet {
 //		}
 //	}
 
-    public InitReverseSet() throws Exception {
-        long startTime = System.currentTimeMillis();
-        //change();
-        getPath();
-        long endTime = System.currentTimeMillis();
-        System.out.println("程序运行时间： " + (endTime - startTime) / 1000.0 + "s");
-    }
+	public InitReverseSet() throws Exception {
+            long startTime=System.currentTimeMillis();
+            //change();
+            getPath();
+            long endTime=System.currentTimeMillis();
+            System.out.println("程序运行时间： "+(endTime-startTime)/1000.0 +"s");
+	}
 
-    public static BigInteger HashString(String key) {
-        int ch;
-        int len = key.length(), i;
-        BigInteger Hash = BigInteger.valueOf(0);
-        for (i = 0; i < len; i++) {
-            ch = key.charAt(i);
-            Hash = Hash.multiply(BigInteger.valueOf(33)).add(BigInteger.valueOf((long) ch));
-        }
-        return Hash;
-    }
-
-    public void getPath() throws Exception {
-        NewSearch.ReadJson();
-        int counter = 0;
-        for (String key : fileMap.get(".txt").keySet()) {
-            counter += fileMap.get(".txt").get(key).size();
-        }
-        threadSignal = new CountDownLatch(counter);
-        fileMap.get(".txt").keySet().stream().forEach((s1) -> {
-            fileMap.get(".txt").get(s1).stream().forEach(s2 -> {
-                try {
-                    new Thread(new ReadFile(new File(s1 + "\\" + s2), threadSignal)).start();
-                } catch (Exception ex) {
-                    Logger.getLogger(InitReverseSet.class.getName()).log(Level.SEVERE, null, ex);
-                }
+	public static BigInteger HashString(String key) {
+		int ch;
+		int len = key.length(), i;
+		BigInteger Hash = BigInteger.valueOf(0);
+		for (i = 0; i < len; i++) {
+			ch = key.charAt(i);
+			Hash = Hash.multiply(BigInteger.valueOf(33)).add(BigInteger.valueOf((long) ch));
+		}
+		return Hash;
+	}
+        public void getPath()  throws Exception{
+            NewSearch.ReadJson();
+            int counter = 0;
+            for (String  key:fileMap.get(".txt").keySet())
+                counter += fileMap.get(".txt").get(key).size();
+            threadSignal = new CountDownLatch(counter);
+            fileMap.get(".txt").keySet().stream().forEach((s1) -> {
+                fileMap.get(".txt").get(s1).stream().forEach(s2 -> {
+                    try {
+                        new Thread(new ReadFile(new File(s1 + "\\" + s2),threadSignal)).start();
+                    } catch (Exception ex) {
+                        Logger.getLogger(InitReverseSet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                });
             });
-        });
-        threadSignal.await();
-        File fileJson = new File("setFile/dict.json");
-        System.out.println("搜索完毕，开始写入json！");
-        String jsonString = JSON.toJSONString(data);
-        try (BufferedWriter br = new BufferedWriter(new FileWriter(fileJson))) {
-            br.write(jsonString);
-            System.out.println("json写入完成！");
+            threadSignal.await();
+            File fileJson = new File("gui/backtable/dict.json");
+            System.out.println("搜索完毕，开始写入json！");
+            String jsonString = JSON.toJSONString(data);
+            try (BufferedWriter br = new BufferedWriter(new FileWriter(fileJson))) {
+                br.write(jsonString);
+                System.out.println("json写入完成！");
+            }
+            fileJson = new File("gui/backtable/data.json");
+            System.out.println("搜索完毕，开始写入json！");
+            jsonString = JSON.toJSONString(Paths);
+            try (BufferedWriter br = new BufferedWriter(new FileWriter(fileJson))) {
+                br.write(jsonString);
+                System.out.println("json写入完成！");
+            }
         }
-        fileJson = new File("setFile/data.json");
-        System.out.println("搜索完毕，开始写入json！");
-        jsonString = JSON.toJSONString(Paths);
-        try (BufferedWriter br = new BufferedWriter(new FileWriter(fileJson))) {
-            br.write(jsonString);
-            System.out.println("json写入完成！");
-        }
-    }
 //        public void test(File file) throws Exception{
 //            ArrayList<String> words = new ArrayList<>();
 //            String filePath = file.getPath();
@@ -84,55 +81,50 @@ public class InitReverseSet {
 //            Analyze.testCJK(fileContent, words);
 //            Add(words,filePath);
 //        }
-
-    public class ReadFile implements Runnable {
-
-        private File file;
-        private ArrayList<String> words = new ArrayList<>();
-        private CountDownLatch threadsSignal;
-
-        public ReadFile(File file, CountDownLatch threadsSignal) {
-            this.file = file;
-            this.threadsSignal = threadsSignal;
-        }
-
-        public void run() {
-            try {
-                System.out.println(Thread.currentThread().getName() + "开始...");
-                String filePath = file.getPath();
-                String fileEncode = EncodingDetect.getJavaEncode(filePath);
-                String fileContent = FileUtils.readFileToString(new File(filePath), fileEncode);
-                if (fileContent.length() <= 1000000) {
-                    Analyze.testCJK(fileContent, words);
-                    Paths.add(filePath);
-                    Add(words, Paths.indexOf(filePath));
+        public class ReadFile implements Runnable {
+            private File file;
+            private ArrayList<String> words = new ArrayList<>();
+            private CountDownLatch threadsSignal;  
+            public ReadFile(File file,CountDownLatch threadsSignal) {
+                this.file = file;
+                this.threadsSignal = threadsSignal;
+            }
+            public void run() {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "开始...");  
+                    String filePath = file.getPath();
+                    String fileEncode = EncodingDetect.getJavaEncode(filePath);
+                    String fileContent = FileUtils.readFileToString(new File(filePath), fileEncode);
+                    if (fileContent.length()<=1000000) {
+                        Analyze.testCJK(fileContent, words);
+                        Paths.add(filePath);
+                        Add(words,Paths.indexOf(filePath));
+                    }
+                    threadsSignal.countDown();
+                    System.out.println(Thread.currentThread().getName() + "结束. 还有" + threadsSignal.getCount() + " 个线程"); 
+                } catch (Exception ex) {
+                    //Logger.getLogger(ReverseSet.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                threadsSignal.countDown();
-                System.out.println(Thread.currentThread().getName() + "结束. 还有" + threadsSignal.getCount() + " 个线程");
-            } catch (Exception ex) {
-                //Logger.getLogger(ReverseSet.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-    }
-
-    public synchronized void Add(ArrayList<String> words, int Num) {
-        for (String word : words) {
-            BigInteger Hashc = HashString(word);
-            BigInteger No;
-            No = new BigInteger(String.valueOf(Num));
-            if (data.containsKey(Hashc)) {
-                if (!data.get(Hashc).contains(No)) {
-                    data.get(Hashc).add(No);
-                }
-            } else {
-                ArrayList<BigInteger> temp = new ArrayList<>();
-                temp.add(No);
-                data.put(Hashc, temp);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        new InitReverseSet();
-    }
+       public synchronized void Add(ArrayList<String> words,int Num) {
+           for (String word:words) {
+               BigInteger Hashc = HashString(word);
+               BigInteger No;
+               No = new BigInteger(String.valueOf(Num));
+               if (data.containsKey(Hashc)) {
+                   if (!data.get(Hashc).contains(No)) {
+                       data.get(Hashc).add(No);
+                   } 
+               }
+               else {
+                   ArrayList<BigInteger> temp = new ArrayList<>();
+                   temp.add(No);
+                   data.put(Hashc,temp);
+               }
+           }
+       }
+      public static void main(String[] args) throws Exception{
+          new InitReverseSet();
+      }
 }
--- gui/backtable/SearchContent.java
+++ gui/backtable/SearchContent.java
@@ -13,34 +13,33 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
- import com.sun.webkit.ContextMenu.ShowContext;
+import com.sun.webkit.ContextMenu.ShowContext;
 
- import backtable.ReverseSet;*/
+import backtable.ReverseSet;*/
+
 public class SearchContent {
-
     private BigInteger hash;
-    private HashMap<BigInteger, ArrayList<BigInteger>> data = new HashMap<>();
-    private ArrayList<String> Paths = new ArrayList<>();
+    private HashMap<BigInteger,ArrayList<BigInteger>> data = new HashMap<>();
+    private  ArrayList<String> Paths = new ArrayList<>();
     public ArrayList<String> result = new ArrayList<>();
-    BigInteger target;
-
-    public SearchContent(String target) throws Exception {
+    BigInteger  target;
+    public SearchContent(String target)  throws Exception{
         hash = ReverseSet.HashString(target);
         String jsonString;
         this.target = ReverseSet.HashString(target);
-        File fileJson = new File("setFile/data.json");
+        File fileJson = new File("gui/backtable/data.json");
         try (BufferedReader br = new BufferedReader(new FileReader(fileJson))) {
             jsonString = br.readLine();
         }
         Paths = JSON.parseObject(jsonString, new TypeReference<ArrayList<String>>() {
         });
-        fileJson = new File("setFile/dict.json");
+        fileJson = new File("gui/backtable/dict.json");
         try (BufferedReader br = new BufferedReader(new FileReader(fileJson))) {
             jsonString = br.readLine();
         }
-        data = JSON.parseObject(jsonString, new TypeReference<HashMap<BigInteger, ArrayList<BigInteger>>>() {
+        data = JSON.parseObject(jsonString, new TypeReference<HashMap<BigInteger,ArrayList<BigInteger>>>() {
         });
-        for (BigInteger key : data.get(target)) {
+        for (BigInteger key:data.get(target)){
             result.add(Paths.get(key.intValue()));
         }
     }
