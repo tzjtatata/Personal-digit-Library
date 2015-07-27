2672713de1e56c8a4b88150617bf7a8bae3d8ba8
feihuanwuying <1397645429@qq.com>
2015-07-27 12:39:35 +0800

    透明的ZComboBox

--- src/gui/ZCalendar.java
+++ src/gui/ZCalendar.java
@@ -5,8 +5,6 @@
  */
 package gui;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
@@ -23,16 +21,18 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.plaf.basic.BasicComboBoxUI;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.sun.xml.internal.fastinfoset.tools.StAX2SAXReader;
+import java.awt.Component;
 
 /**
  *
@@ -40,30 +40,30 @@ import javax.swing.plaf.basic.BasicComboBoxUI;
  */
 public class ZCalendar extends JPanel {
 
-    private JLabel dateJLabel, noteJLabel;  //后两个分别用来提示日期和笔记等
+    private JLabel dateJLabel, noteJLabel; // 后两个分别用来提示日期和笔记等
     private JLabel saveLabel, cleanLabel, queryLabel;
-    private JLabel yearJLabel, monthJLabel;  //年字和月字
-    private JLabel[] dateJLabels, weekJLabels;  //42个日期块和7个星期块
-    private JComboBox<String> yearBox, monthBox;  //两个下拉框
-    private JTextArea noteArea;  //笔记
-    private Calendar systemCalendar;  //系统时间
-    private JScrollPane noteJScrollPane; //控制文本域的滚动条
-    public static HashMap<String, String> noteMap;  //笔记对应的hashmap
-    private int[] selected;  //当前被选择的年月，和日期块序号
+    private JLabel yearJLabel, monthJLabel; // 年字和月字
+    private JLabel[] dateJLabels, weekJLabels; // 42个日期块和7个星期块
+    private JTextArea noteArea; // 笔记
+    private Calendar systemCalendar; // 系统时间
+    private JScrollPane noteJScrollPane; // 控制文本域的滚动条
+    public static HashMap<String, String> noteMap; // 笔记对应的hashmap
+    private int[] selected; // 当前被选择的年月，和日期块序号
+    private ZComboBox yearComboBox, monthComboBox; // 新的Z下拉框
 
     public ZCalendar() throws Exception {
         InitComponents();
     }
 
-    //初始化各种组件
+    // 初始化各种组件
     private void InitComponents() throws Exception {
         this.setLayout(null);
         initMap();
-        //实例化对象
+        // 实例化对象
         systemCalendar = Calendar.getInstance();
-        saveLabel = new JLabel("存储");
-        cleanLabel = new JLabel("清除");
-        queryLabel = new JLabel("查询");
+        saveLabel = new JLabel("存储", JLabel.CENTER);
+        cleanLabel = new JLabel("清除", JLabel.CENTER);
+        queryLabel = new JLabel("查询", JLabel.CENTER);
         dateJLabel = new JLabel("日期", JLabel.LEFT);
         noteJLabel = new JLabel("笔记", JLabel.LEFT);
         dateJLabels = new JLabel[42];
@@ -73,26 +73,55 @@ public class ZCalendar extends JPanel {
         weekJLabels = new JLabel[7];
         yearJLabel = new JLabel("年", JLabel.CENTER);
         monthJLabel = new JLabel("月", JLabel.CENTER);
-        String[] weekStrings = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String[] weekStrings = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri",
+            "Sat"};
         for (int i = 0; i < weekStrings.length; i++) {
             weekJLabels[i] = new JLabel(weekStrings[i], JLabel.CENTER);
         }
-        String[] years = new String[81];  //1970到2050
+        String[] years = new String[81]; // 1970到2050
         for (int i = 0; i < years.length; i++) {
             years[i] = String.valueOf(1970 + i);
         }
-        yearBox = new JComboBox<>(years);
-        String[] months = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-        monthBox = new JComboBox<>(months);
+
+        yearComboBox = new ZComboBox(years);
+        String[] months = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12"};
+        monthComboBox = new ZComboBox(months);
         noteArea = new JTextArea();
-        noteJScrollPane = new JScrollPane(noteArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        //设置组件,顺手add
+        noteJScrollPane = new JScrollPane(noteArea,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // 设置组件,顺手add
+        monthComboBox.setBounds(82, 5, 44, 21);
+        monthComboBox.setTextSize(44, 21);
+        monthComboBox.setOpaque(false);
+        monthComboBox.setSelectedItem(String.valueOf(systemCalendar.get(Calendar.MONTH) + 1));
+        monthComboBox.setBorder();
+        this.add(monthComboBox);
+        yearComboBox.setBounds(0, 5, 60, 21);
+        yearComboBox.setTextSize(60, 21);
+        yearComboBox.setOpaque(false);
+        yearComboBox.setSelectedItem(String.valueOf(systemCalendar
+                .get(Calendar.YEAR)));
+        yearComboBox.setBorder();
+        this.add(yearComboBox);
+
+        monthJLabel.setBounds(124, 5, 20, 21);
+        this.add(monthJLabel);
+        yearJLabel.setBounds(64, 5, 14, 21);
+        this.add(yearJLabel);
+        for (int i = 0; i < dateJLabels.length; i++) {
+            dateJLabels[i].setBounds(29 * (i % 7), 68 + 29 * (i / 7), 29, 29);
+            dateJLabels[i].setFont(new java.awt.Font("Leto", 1, 12));
+            this.add(dateJLabels[i]);
+        }
         saveLabel.setBounds(118, 252, 40, 20);
         this.add(saveLabel);
         cleanLabel.setBounds(153, 252, 40, 20);
         cleanLabel.setBorder(null);
         this.add(cleanLabel);
-        queryLabel.setBounds(143, 6, 40, 20);
+        queryLabel.setBounds(155, 6, 40, 20);
         queryLabel.setBorder(null);
         this.add(queryLabel);
         for (int i = 0; i < weekJLabels.length; i++) {
@@ -116,35 +145,13 @@ public class ZCalendar extends JPanel {
         noteJScrollPane.setOpaque(false);
         noteJScrollPane.getViewport().setOpaque(false);
         this.add(noteJScrollPane);
-        yearBox.setBounds(0, 5, 55, 21);
-        yearBox.setSelectedItem(String.valueOf(systemCalendar.get(Calendar.YEAR)));  //选择系统时间
-        //yearBox.setOpaque(false);
-        yearBox.setFont(new java.awt.Font("微软雅黑", Font.BOLD, 10));
-        yearBox.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
-        this.add(yearBox);
-        monthBox.setBounds(77, 5, 39, 21);
-        monthBox.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
-        monthBox.setSelectedItem(String.valueOf(systemCalendar.get(Calendar.MONTH) + 1));
-        monthBox.setFont(new Font("微软雅黑", Font.BOLD, 10));
-        this.add(monthBox);
-        monthJLabel.setBounds(120, 5, 20, 21);
-        monthJLabel.setFont(new java.awt.Font("微软雅黑", 1, 14));
-        this.add(monthJLabel);
-        yearJLabel.setBounds(59, 5, 14, 21);
-        yearJLabel.setFont(new java.awt.Font("微软雅黑", 1, 14));
-        this.add(yearJLabel);
-        for (int i = 0; i < dateJLabels.length; i++) {
-            dateJLabels[i].setBounds(29 * (i % 7), 68 + 29 * (i / 7), 29, 29);
-            dateJLabels[i].setFont(new java.awt.Font("Leto", 1, 12));
-            this.add(dateJLabels[i]);
-        }
-        //监听器响应阶段
+        // 监听器响应阶段
         queryLabel.addMouseListener(new CursorListener());
         queryLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if (Integer.parseInt((String) yearBox.getSelectedItem()) != selected[0]
-                        || Integer.parseInt((String) monthBox.getSelectedItem()) != selected[1]) {  //年月日变化才调用函数
+                if (Integer.parseInt((String) yearComboBox.getSelectedItem()) != selected[0]
+                        || Integer.parseInt((String) monthComboBox.getSelectedItem()) != selected[1]) { // 年月日变化才调用函数
                     setDate(0);
                 }
             }
@@ -157,7 +164,8 @@ public class ZCalendar extends JPanel {
                 try {
                     saveNote();
                 } catch (Exception ex) {
-                    Logger.getLogger(ZCalendar.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(ZCalendar.class.getName()).log(
+                            Level.SEVERE, null, ex);
                 }
             }
         });
@@ -167,8 +175,37 @@ public class ZCalendar extends JPanel {
                 try {
                     cleanNote();
                 } catch (Exception ex) {
-                    Logger.getLogger(ZCalendar.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(ZCalendar.class.getName()).log(
+                            Level.SEVERE, null, ex);
                 }
+            }
+        });
+        yearComboBox.addClickListener(monthComboBox);
+        monthComboBox.addClickListener(yearComboBox);
+        for (Component component : this.getComponents()) {
+            if (component instanceof ZComboBox) {
+            } else {
+                component.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        yearComboBox.setListUnVisible();
+                        monthComboBox.setListUnVisible();
+                    }
+                });
+            }
+        }
+        noteArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                yearComboBox.setListUnVisible();
+                monthComboBox.setListUnVisible();
+            }
+        });
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                yearComboBox.setListUnVisible();
+                monthComboBox.setListUnVisible();
             }
         });
         this.setOpaque(false);
@@ -180,26 +217,30 @@ public class ZCalendar extends JPanel {
      * isFirst:是否是第一次调用 根据已选择日期显示日期标签
      */
     private void setDate(int isFirst) {
-        int year = Integer.parseInt((String) yearBox.getSelectedItem());
-        int month = Integer.parseInt((String) monthBox.getSelectedItem()) - 1; //month从0开始
+        int year = Integer.parseInt((String) yearComboBox.getSelectedItem());
+        int month = Integer.parseInt((String) monthComboBox.getSelectedItem()) - 1; // month从0开始
         Calendar c = Calendar.getInstance();
         c.set(year, month, 1);
-        int first = c.get(Calendar.DAY_OF_WEEK) - 1;  //第一天的位置（周日视为0）
+        int first = c.get(Calendar.DAY_OF_WEEK) - 1; // 第一天的位置（周日视为0）
         for (JLabel dateJLabel1 : dateJLabels) {
-            dateJLabel1.setVisible(false);  //全部消失
+            dateJLabel1.setVisible(false); // 全部消失
         }
         for (int i = first, j = 1; j < c.getActualMaximum(Calendar.DATE) + 1; i++, j++) {
-            dateJLabels[i].setVisible(true);  //部分显现并设置显示内容
+            dateJLabels[i].setVisible(true); // 部分显现并设置显示内容
             dateJLabels[i].setText(String.valueOf(j));
             if (dateJLabels[i].getMouseListeners() != null) {
-                for (MouseListener mouseListener : dateJLabels[i].getMouseListeners()) {
-                    dateJLabels[i].removeMouseListener(mouseListener);  //先移除之前的监听器
+                for (MouseListener mouseListener : dateJLabels[i]
+                        .getMouseListeners()) {
+                    dateJLabels[i].removeMouseListener(mouseListener); // 先移除之前的监听器
                 }
             }
-            dateJLabels[i].addMouseListener(new DateListener(year, month + 1, j));
+            dateJLabels[i]
+                    .addMouseListener(new DateListener(year, month + 1, j));
         }
-        if (year == systemCalendar.get(Calendar.YEAR) && month == systemCalendar.get(Calendar.MONTH)) {  //为当前月则突出当前日期
-            //dateJLabels[first + systemCalendar.get(Calendar.DATE) - 1].setOpaque(true);
+        if (year == systemCalendar.get(Calendar.YEAR)
+                && month == systemCalendar.get(Calendar.MONTH)) { // 为当前月则突出当前日期
+            // dateJLabels[first + systemCalendar.get(Calendar.DATE) -
+            // 1].setOpaque(true);
         } else {
             for (JLabel dateJLabel1 : dateJLabels) {
                 dateJLabel1.setOpaque(false);
@@ -226,21 +267,23 @@ public class ZCalendar extends JPanel {
         Calendar c = Calendar.getInstance();
         c.set(year, month - 1, date);
         int i = 0;
-        while (!dateJLabels[i++].isVisible());
-        i--; //计算第一个显示了的日期
-        if (selected[2] != -1) {  //是-1说明是第一次调用
+        while (!dateJLabels[i++].isVisible())
+			;
+        i--; // 计算第一个显示了的日期
+        if (selected[2] != -1) { // 是-1说明是第一次调用
             dateJLabels[selected[2]].setForeground(SetUp.FORE_COLOR);
             selected[2] = date + i - 1;
-            dateJLabels[selected[2]].setForeground(SetUp.SPECIAL_COLOR);  //选中后前景色突出
+            dateJLabels[selected[2]].setForeground(SetUp.SPECIAL_COLOR); // 选中后前景色突出
         } else {
             int j = 0;
-            while (++j != date);
+            while (++j != date)
+				;
             j--;
-            selected[2] = i + j;  //当前日期所代表序号
+            selected[2] = i + j; // 当前日期所代表序号
             dateJLabels[selected[2]].setForeground(SetUp.SPECIAL_COLOR);
         }
 
-        if (noteMap.containsKey(year + "-" + month + "-" + date)) {  //如果有记事的话
+        if (noteMap.containsKey(year + "-" + month + "-" + date)) { // 如果有记事的话
             noteArea.setText(noteMap.get(year + "-" + month + "-" + date));
             noteJLabel.setText("当日读书笔记");
         } else {
@@ -257,8 +300,11 @@ public class ZCalendar extends JPanel {
         if ("".equals(noteArea.getText())) {
             noteJLabel.setText("请输入内容!");
         } else {
-            noteMap.put(yearBox.getSelectedItem() + "-" + monthBox.getSelectedItem()
-                    + "-" + dateJLabels[selected[2]].getText(), noteArea.getText());  //年-月-日为key，信息为value
+            noteMap.put(
+                    yearComboBox.getSelectedItem() + "-"
+                    + monthComboBox.getSelectedItem() + "-"
+                    + dateJLabels[selected[2]].getText(),
+                    noteArea.getText()); // 年-月-日为key，信息为value
             noteJLabel.setText("存储成功!");
             writeJson();
         }
@@ -273,8 +319,9 @@ public class ZCalendar extends JPanel {
         if (!"".equals(noteArea.getText())) {
             noteArea.setText("");
             noteJLabel.setText("清除成功");
-            noteMap.remove(yearBox.getSelectedItem() + "-" + monthBox.getSelectedItem()
-                    + "-" + dateJLabels[selected[2]].getText());
+            noteMap.remove(yearComboBox.getSelectedItem() + "-"
+                    + monthComboBox.getSelectedItem() + "-"
+                    + dateJLabels[selected[2]].getText());
             writeJson();
         }
     }
@@ -285,20 +332,23 @@ public class ZCalendar extends JPanel {
      */
     private void initMap() throws Exception {
         File f = new File("setFile/calendarNote.json");
-        if (!f.exists()) {  //没有就创建新文件
+        if (!f.exists()) { // 没有就创建新文件
             f.createNewFile();
             noteMap = new HashMap<>();
         } else {
             String line = "";
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF-8"))) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(f), "UTF-8"))) {
                 line = br.readLine();
             }
-            if (JSON.parseObject(line, new TypeReference<HashMap<int[], String>>() {
-            }) == null) {
+            if (JSON.parseObject(line,
+                    new TypeReference<HashMap<int[], String>>() {
+                    }) == null) {
                 noteMap = new HashMap<>();
             } else {
-                noteMap = JSON.parseObject(line, new TypeReference<HashMap<String, String>>() {
-                });  //转换为map
+                noteMap = JSON.parseObject(line,
+                        new TypeReference<HashMap<String, String>>() {
+                        }); // 转换为map
             }
         }
     }
@@ -309,7 +359,8 @@ public class ZCalendar extends JPanel {
     private void writeJson() throws Exception {
         displayNote();
         File f = new File("setFile/calendarNote.json");
-        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "UTF-8"))) {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(f), "UTF-8"))) {
             bw.write(JSON.toJSONString(noteMap));
         }
     }
@@ -320,7 +371,8 @@ public class ZCalendar extends JPanel {
      */
     public static void displayNote() {
         Calendar c = Calendar.getInstance();
-        String date = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DATE);
+        String date = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1)
+                + "-" + c.get(Calendar.DATE);
         if (!noteMap.containsKey(date)) {
             CalenderJPanel.setText("当日无读书笔记");
         } else {
@@ -335,10 +387,10 @@ public class ZCalendar extends JPanel {
         }
         noteJLabel.setForeground(SetUp.FORE_COLOR);
         noteArea.setForeground(SetUp.FORE_COLOR);
-        yearBox.setForeground(SetUp.FORE_COLOR);
-        yearBox.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
-        monthBox.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
-        monthBox.setForeground(SetUp.FORE_COLOR);
+        yearComboBox.setForeground(SetUp.FORE_COLOR);
+        yearComboBox.setBorder();
+        monthComboBox.setBorder();
+        monthComboBox.setForeground(SetUp.FORE_COLOR);
         yearJLabel.setForeground(SetUp.FORE_COLOR);
         monthJLabel.setForeground(SetUp.FORE_COLOR);
         for (JLabel dateJLabel1 : dateJLabels) {
@@ -379,6 +431,9 @@ public class ZCalendar extends JPanel {
         @Override
         public void mouseClicked(MouseEvent e) {
             setSelect(year, month, date);
+            yearComboBox.setListUnVisible();
+            monthComboBox.setListUnVisible();
         }
     }
+
 }
---
+++ src/gui/ZComboBox.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import com.sun.nio.sctp.AbstractNotificationHandler;
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.Robot;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * JPanel上加上以空参数初始化的JTextField,配合JList以替代JComboBox
+ *
+ * @author ZouKaifa
+ */
+public class ZComboBox extends JPanel {
+
+    private final JTextField textField;
+    private JScrollPane scrollPane;
+    private JList<String> list;
+
+    /**
+     * 用于JList的字符串数组
+     *
+     * @param strings
+     */
+    public ZComboBox(String[] strings) {
+        super();
+        this.setLayout(null);
+        textField = new JTextField();
+        list = new JList<>(strings);
+        scrollPane = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        this.add(scrollPane);
+        scrollPane.setVisible(false);
+        list.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.err.println("?");
+                textField.setText(list.getSelectedValue());
+                scrollPane.setVisible(false);
+                setSize(textField.getWidth(), textField.getHeight());
+            }
+        });
+        textField.setEditable(false);
+        textField.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
+        textField.setOpaque(false);
+        textField.addMouseListener(new TextListener(this));
+        this.add(textField);
+        this.setVisible(true);
+        list.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                setListUnVisible();
+            }
+        });
+        textField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                setListUnVisible();
+            }
+        });
+    }
+
+    /**
+     * 设置textField的大小
+     *
+     * @param width
+     * @param height
+     */
+    public void setTextSize(int width, int height) {
+        textField.setSize(width, height);
+    }
+
+    public void setText(String text) {
+        textField.setText(text);
+    }
+
+    public boolean isListVisible() {
+        return scrollPane.isVisible();
+    }
+
+    public void setListUnVisible() {
+        scrollPane.setVisible(false);
+        setSize(textField.getWidth(), textField.getHeight());
+    }
+
+    public void setSelectedItem(String string) {
+        textField.setText(string);
+    }
+
+    public String getSelectedItem() {
+        return textField.getText();
+    }
+
+    public void setBorder() {
+        textField.setBorder(BorderFactory.createLineBorder(SetUp.FORE_COLOR));
+    }
+
+    @Override
+    public void setFont(Font font) {
+        if (textField != null && list != null) {
+            textField.setFont(font);
+            list.setFont(font);
+        }
+    }
+
+    public void addClickListener(ZComboBox another) {
+        textField.addMouseListener(new ClickListener(another));
+    }
+    /*
+     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, AWTException {
+     JFrame jFrame = new JFrame();
+     JPanel rootPanel = new JPanel();
+     jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     rootPanel.setBounds(200, 100, 300, 200);
+     rootPanel.setLayout(null);
+     jFrame.setLayout(null);
+     jFrame.setBounds(200, 100, 300, 200);
+     jFrame.setContentPane(rootPanel);
+     JLabel label = new JLabel("fsdjfhsjdaf?");
+     label.setBounds(0, 60, 130, 20);
+     rootPanel.add(label);
+     ZComboBox zComboBox = new ZComboBox(new String[]{"1", "2", "3", "4", "5", "6", "7", "34", "44", "4", "43", "2332"});
+     zComboBox.setBounds(0, 0, 130, 60);
+     zComboBox.setTextSize(100, 20);
+     zComboBox.setText("1");
+     rootPanel.add(zComboBox);
+     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+     SwingUtilities.updateComponentTreeUI(jFrame);
+     jFrame.setVisible(true);
+     }*/
+
+    class TextListener extends MouseAdapter {
+
+        private final ZComboBox comboBox;
+
+        public TextListener(ZComboBox comboBox) {
+            this.comboBox = comboBox;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (scrollPane.isVisible()) {
+                scrollPane.setVisible(false);
+                comboBox.setSize(textField.getWidth(), textField.getHeight());
+            } else {
+                scrollPane.setBounds(textField.getX(), textField.getY() + textField.getHeight(),
+                        textField.getWidth(), 120);
+                comboBox.setSize(textField.getWidth(), textField.getHeight() + scrollPane.getHeight());
+                list.setSelectedValue(textField.getText(), true);
+                scrollPane.setVisible(true);
+                comboBox.repaint();
+            }
+        }
+    }
+
+    class ClickListener extends MouseAdapter {
+
+        private final ZComboBox another;
+
+        public ClickListener(ZComboBox another) {
+            this.another = another;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent event) {
+            another.setListUnVisible();
+        }
+    }
+}
